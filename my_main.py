533eadccb7efb2c53fe88e0aaeede7a3df6208aa
Admin <xanilegendx@gmail.com>
2020-05-06 00:55:04 +0300

    Ynet making full experiment with pretrain and baseline

--- my_main.py
+++ my_main.py
@@ -1,12 +1,11 @@
 import os
-import random
 import argparse
 import datetime
 
 import pandas as pd
-from pathlib import Path
 
 from train import train
+from Utils.constants import SEED_LIST, TRAIN_TRAIN_NUMBER, TRAIN_VALID_NUMBER, PRETRAIN, YNET, BASELINE, K1, K2
 from Utils.utils import print_save_results
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -15,40 +14,45 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg('--root', type=str, default='runs/debug')
     arg('--N', type=int, default=1)
-    arg('--pretrained', action='store_true')           # if --pretrained parameter then True
+    arg('--pretrained', action='store_true')
     arg('--lr', type=float, default=0.0001)
     arg('--batch_size', type=int, default=1)
-    arg('--augment_list', nargs='+', default=[])
     arg('--image_path', type=str, default='./Data/h5/')
-    arg('--n_epochs', type=int, default=1)
-    arg('--prob', type=float, nargs='*', default=[0.1])
     arg('--attribute', nargs='+', default=[])
     arg('--cuda1', action='store_true')
     arg('--workers', type=int, default=1)
     arg('--resume', action='store_true')
     arg('--show_model', action='store_true')
-    arg('--save_model', action='store_true')
     arg('--model_path', type=str, default='/Data/model/')
     arg('--normalize', action='store_true')
     args = parser.parse_args()
 
-    root = Path(args.root)
-    root.mkdir(exist_ok=True, parents=True)
-
-    results = pd.DataFrame()
+    results1 = pd.DataFrame()
+    results2 = pd.DataFrame()
+    results3 = pd.DataFrame()
 
     N = args.N
 
     time = datetime.datetime.now().strftime('%d_%H.%M')
     i = 0
-    r = random.Random(0)
-    SEED_LIST = [r.randint(1, 500) for _ in range(10)]
 
-    for experiment in range(N):
+    for experiment in range(0, N):
+        SEED = SEED_LIST[experiment]
         args.N = experiment
         print(args)
-        results = train(args, results, SEED=SEED_LIST[experiment])
-        print_save_results(args, results, root, i, time)
-        i += 1
+
+        print('_' * 40)
+        print('pretrain resnet50 on {} for {} epoch'.format(TRAIN_TRAIN_NUMBER, K1))
+        results1 = train(args, results1, SEED=SEED, train_type=PRETRAIN, epochs=K1)
+        print_save_results(args, results1, time, PRETRAIN)
+
+        print('_' * 40)
+        print('train y_net on {} for {} epoch'.format(TRAIN_TRAIN_NUMBER+TRAIN_VALID_NUMBER, K2))
+        results2 = train(args, results2, SEED=SEED, train_type=YNET, epochs=K2)
+        print_save_results(args, results2, time, YNET)
+
+        print('_' * 40)
+        print('train resnet50 on {} for {} epoch to compare'.format(TRAIN_TRAIN_NUMBER+TRAIN_VALID_NUMBER, K1+K2))
+        results3 = train(args, results3, SEED=SEED, train_type=BASELINE, epochs=K1+K2)
+        print_save_results(args, results3, time, BASELINE)
